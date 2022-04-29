7d1215d8d26e830d931c12d3aa3b10d391cb075a
Hugo Rios <hugostaq@gmail.com>
2022-04-29 01:29:58 -0300

    Minor Test Changes
    Minor Test Changes

--- app/src/main/java/com/example/erodad20sheet/fragments/CharNotesFragment.kt
+++ app/src/main/java/com/example/erodad20sheet/fragments/CharNotesFragment.kt
@@ -1,20 +1,17 @@
 package com.example.erodad20sheet.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.erodad20sheet.CharacterSheetActivity
+import com.example.erodad20sheet.NotesAddActivity
 import com.example.erodad20sheet.R
-import com.example.erodad20sheet.adapter.NotesAdapter
-import com.example.erodad20sheet.model.NotesDataClass
-import kotlinx.android.synthetic.main.adapter_notes.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_notes_add.*
+import kotlinx.android.synthetic.main.fragment_char_notes.*
 
 class CharNotesFragment : Fragment() {
 
@@ -33,20 +30,10 @@ class CharNotesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val note1 = NotesDataClass("Primeira Nota", "Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
-                "Lorem Ipsum Lorem Ipsum")
-        val note2 = NotesDataClass("Segunda Nota", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
-                "Lorem Ipsum Lorem Ipsum Lorem Ipsum Ipsum Lorem Ipsum Ipsum")
-        val note3 = NotesDataClass("Terceira Nota", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
-                "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
-                "Lorem Ipsum Lorem Ipsum")
-        val notesArrayList = arrayListOf(note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3,
-            note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3,
-            note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3, note1, note2, note3)
-
-        val notesRecyclerView = view.findViewById<RecyclerView>(R.id.notes_recycler)
-        notesRecyclerView.layoutManager = LinearLayoutManager(view.context)
-        notesRecyclerView.adapter = NotesAdapter(notesArrayList)
+        add_note_fab.setOnClickListener {
+            val intent = Intent(view.context, NotesAddActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
